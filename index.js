17de63b978f0949b085ab110d62983a5b2ceb199
jlom2024 <jlom2002@gmail.com>
2025-05-18 01:47:40 -0500

    Listen

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.post('/enviar-mensaje', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor AgendaKIA activo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor AgendaKIA activo en el puerto ${PORT}`);
 });
